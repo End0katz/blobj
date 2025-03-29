343bc4a22e828def2f3f683dbd4a16f6feb748e4
End0katz <endokatz@gmail.com>
2025-03-29 14:09:38 +0000

    Created the @Unstable threads package

---
+++ src/main/java/com/end0katz/blobj/threads/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * A package for some multi-threading utilities.
+ *
+ * My first time working with multithreading in java, so not
+ * recommended to touch unless you are debugging.
+ */
+@Unstable
+package com.end0katz.blobj.threads;
+
+import com.end0katz.blobj.annotations.*;
