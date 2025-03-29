cb534b4df709d4b2e13911dc77ab7ab49f8582e5
End0katz <endokatz@gmail.com>
2025-03-29 11:34:40 +0000

    Forgot punctuation in com.end0katz.blobj.vec.package-info

--- src/main/java/com/end0katz/blobj/vec/package-info.java
+++ src/main/java/com/end0katz/blobj/vec/package-info.java
@@ -2,17 +2,17 @@
  * A class containing some vectors:
  *
  *
- * {@code Vec}s are the interfaces for anything wanting to be a vector
+ * {@code Vec}s are the interfaces for anything wanting to be a vector.
  *
  * {@code Int}s are vectors that implements the {@code Vec} interfaces for
- * {@link Integer}s
+ * {@link Integer}s.
  *
  *
  * {@code Float}s are vectors that implement the {@code Vec} interface for
- * {@link Double}s
+ * {@link Double}s.
  *
  *
  * {@code Obj<T>}s are vectors that implement the {@code Vec} interface for
- * arbitrary {@link Object}s
+ * arbitrary {@link Object}s.
  */
 package com.end0katz.blobj.vec;
