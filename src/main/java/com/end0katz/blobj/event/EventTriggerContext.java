793b17acf80c2de6e617877d2cd3feabe008f224
End0katz <endokatz@gmail.com>
2025-04-02 17:02:55 +0100

    Added javadoc for events (time taken: 3000 yrs)

--- src/main/java/com/end0katz/blobj/event/EventTriggerContext.java
+++ src/main/java/com/end0katz/blobj/event/EventTriggerContext.java
@@ -1,9 +1,17 @@
 package com.end0katz.blobj.event;
 
+/**
+ * The context passed to all {@link Event} subscribers that except it.
+ *
+ * @param event the event that was triggered.
+ * @param name the name of the event.
+ * @param reason the provided reason why the trigger occurred.
+ * @param params arguments passed to the methods.
+ *
+ * @see Event#DEFAULT_REASON
+ */
 public record EventTriggerContext(
-    Event event, String name, String reason, Object[] params
-)
-{
+        Event event, String name, String reason, Object[] params) {
 
     @Override
     public EventTriggerContext clone() {
