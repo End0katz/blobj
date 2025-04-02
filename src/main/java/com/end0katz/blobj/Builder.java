772eb0beb1ec5a491b2183c0397333248ff116cc
End0katz <endokatz@gmail.com>
2025-04-02 15:26:47 +0100

    Edited .github/workflows/maven.yml to (hopefully) stop ci failure.

--- src/main/java/com/end0katz/blobj/Builder.java
+++ src/main/java/com/end0katz/blobj/Builder.java
@@ -7,5 +7,12 @@ package com.end0katz.blobj;
  */
 public interface Builder<T> {
 
+    /**
+     * Build the object.
+     *
+     * @return the build Object
+     * @implNote unless not possible or not applicable, the builder should be
+     * reusable. Aside from if the class
+     */
     T build();
 }
