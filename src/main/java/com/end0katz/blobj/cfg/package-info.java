f32de7eef073353171955f3e0418cc3173f73aaa
End0katz <endokatz@gmail.com>
2025-04-03 08:36:43 +0100

    Added the cfg package

---
+++ src/main/java/com/end0katz/blobj/cfg/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Package For Configuration File Formats:
+ *
+ * @plannedFeature XML (Using Jsoup)
+ * @plannedFeature YML (Probably using SnakeYAML)
+ * @plannedFeature JSON (Probably using google json library)
+ * @plannedFeature TOML (Provably using
+ * <a href="https://github.com/mwanji/toml4j">mwanji/toml4j</a>)
+ *
+ * @plannedFeature Specifications/Schemas for tree-like conf formats (json, yml,
+ * toml?, xml?)
+ */
+package com.end0katz.blobj.cfg;
