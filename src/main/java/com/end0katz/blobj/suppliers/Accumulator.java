3bd177351731a49bfc637b745d930eaf538d7004
End0katz <endokatz@gmail.com>
2025-04-04 16:18:08 +0100

    Updated javadoc

--- src/main/java/com/end0katz/blobj/suppliers/Accumulator.java
+++ src/main/java/com/end0katz/blobj/suppliers/Accumulator.java
@@ -2,24 +2,54 @@ package com.end0katz.blobj.suppliers;
 
 import java.util.function.*;
 
+/**
+ * {@link IntSupplier} that increments a value by a certain amount each time.
+ */
 public class Accumulator implements IntSupplier, Supplier<Integer> {
 
+    /**
+     * Construct a basic accumulator: starts at 0, increasing by 1 each time.
+     */
     public Accumulator() {
         this(1);
     }
 
+    /**
+     * Construct an accumulator with custom accumulation value.
+     *
+     * @param acc the amount to change by each time.
+     */
     public Accumulator(int acc) {
         this(0, acc);
     }
 
+    /**
+     * An accumulator starting at, and incrementing by, a custom value.
+     *
+     * @param start the initial value, and first return value of
+     * {@link Accumulator#get()} &amp; {@link Accumulator#getAsInt()}.
+     * @param acc the amount to change by each time.
+     */
     public Accumulator(int start, int acc) {
         this.i = start;
         this.acc = acc;
     }
 
+    /**
+     * The current value
+     */
     private int i;
+    /**
+     * The amount to change by
+     */
     private int acc;
 
+    /**
+     * Get the next return value of {@link Accumulator#get()} without
+     * incrementing.
+     *
+     * @return {@link Accumulator#i}.
+     */
     public int peek() {
         return i;
     }
