facaaf0ee20e3c61b83bad61e89a2a4da21cc13b
End0katz <endokatz@gmail.com>
2025-04-04 16:08:28 +0100

    Added the misc package

---
+++ src/main/java/com/end0katz/blobj/misc/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Packages and classes in this class are things I do not expect to be used very often, if at all. (assuming this
+ * <i>library</i> gets used at all)
+ */
+package com.end0katz.blobj.misc;
